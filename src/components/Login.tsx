f6bdb2638b4030ce33788bd4fe8dd89fd2fd999c
hoonVelop-shin <78241389+hoonVelop-shin@users.noreply.github.com>
2021-08-05 20:03:05 +0900

    background-image 추가

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Content>
+        <BgImage />
+      </Content>
+    </Container>
+  );
 };
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  background-image: url("/images/login-background.jpeg");
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: -1;
+`;
 
 export default Login;
