7ed6a19a40b315763601af26a5af5e26760301ec
hoonVelop-shin <78241389+hoonVelop-shin@users.noreply.github.com>
2021-08-05 19:48:19 +0900

    styled-components and router-dom ㅅㅓㄹ치

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components";
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
