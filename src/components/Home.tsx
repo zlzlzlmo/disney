1abbe231b78cdd60b5fff4c546696f88e0f7f4e0
hoonVelop-shin <78241389+hoonVelop-shin@users.noreply.github.com>
2021-08-06 00:26:58 +0900

    viewer추가

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 const Home = () => {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
     </Container>
   );
 };
